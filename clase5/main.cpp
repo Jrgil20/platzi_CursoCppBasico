e7843a26d8f1e35558d8b402903b45e3a9915a48
JRGIL12 <fariasjr223@gmail.com>
2024-11-17 01:53:35 -0400

    Add main.cpp: Implement basic "Hello, World!" program in C++

---
+++ clase5/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Incluimos la librería iostream
+
+using namespace std; // Utilizamos el espacio de nombres estándar
+
+int main() {
+    cout << "Hola, mundo!" << endl; // Mostramos un texto en pantalla
+    return 0; // Indicamos que el programa terminó correctamente
+}
